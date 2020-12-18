fc1ce5dad3b181521018fb78b061702edb196ea3
reginaxy <regina6819@gmail.com>
2020-12-18 23:26:08 +0800

    regina is here

--- MusicPlayer/Source.cpp
+++ MusicPlayer/Source.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void main() {
 	cout << "hello this is a test";
+	cout << "Hello. Regina is here";
 	//if you see this try to change something
 	//commit first, then push all
 	//remeber to pull also
